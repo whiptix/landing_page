61dc84e5e84783cce28e888aa1641a1d791e1bb8
whiptix <whiptix@gmail.com>
2025-06-14 15:04:05 +0300

    renamed app

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Thamani Crew',
+  description: 'Thamani Crew',
+  generator: 'Thamani Crew',
 }
 
 export default function RootLayout({
